a7fb4ab8e0d06d5881da25a45fa3dcd87da7129d
Stathis <alieus@hotmail.gr>
2021-06-27 18:30:31 +0300

    Implement locking transaction mechanism

--- controllers/dice.js
+++ controllers/dice.js
@@ -1,16 +1,55 @@
 const crypto = require("crypto");
 // const value = crypto.createHash('sha256').update(pwd).digest('hex');
+const TIMEOUT = 5000;
+
+function acquireLock(app) {
+    if (app.transactionUID && app.transactionStartTime + TIMEOUT > Date.now()) {
+        return false; // still waiting for previous transaction
+    }
+
+    app.transactionUID = crypto.randomBytes(16).toString("hex");
+    app.transactionStartTime = Date.now();
+
+    return true;
+}
+
+function releaseLock(app) {
+    app.transactionUID = null;
+}
+
+function checkLock(req) {
+    if (req.app.transactionStartTime + TIMEOUT < Date.now()) {
+        // lock expired -> release it
+        releaseLock(req.app);
+        return false;
+    }
+
+    return req.query.transactionUID === req.app.transactionUID;
+}
 
 function initiate(req, res) {
+    if (!acquireLock(req.app)) {
+        return res.status(509).json({ error: "Server Busy" });
+    }
+
     const r2 = crypto.randomBytes(16).toString('hex') + Date.now();
     console.log(r2)
     req.app._r2 = r2;
     req.app._s1 = req.query.s1;
     const s2 = crypto.createHash('sha256').update(""+r2).digest('hex');
-    return res.json({"s2": s2, s1: req.query.s1});
+
+    return res.json({
+        "s2": s2,
+        s1: req.query.s1,
+        transactionUID: req.app.transactionUID,
+    });
 }
 
 function finalize(req, res) {
+    if (!checkLock(req)) {
+        return res.status(400).json({ error: "Invalid transactionUID" });
+    }
+
     const r1 = req.query.r1;
     const r2 = req.app._r2;
     const h = crypto.createHash('sha256').update(r1).digest('hex');
@@ -25,8 +64,11 @@ function finalize(req, res) {
             return res.json({"r2": req.app._r2, "serverDice": serverDice});
         } catch (_e) {
             return res.status(400).json("Invalid parameter");
+        } finally {
+            releaseLock(req.app);
         }
     } else {
+        releaseLock(req.app);
         return res.status(400).json("You are cheating");
     }
     
